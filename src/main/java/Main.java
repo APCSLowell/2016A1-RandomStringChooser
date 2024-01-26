0e0809d4786e96f2a8b700fbb5ee29dbb7f48251
StephenMiner <64863697+StephenMiner@users.noreply.github.com>
2024-01-26 07:00:00 -0800

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,22 @@
-public class Main {
-  public static void main(String[] args) {
-    int [][] pixels1 = {
-      {255, 184, 178,  84, 129},
-      { 84, 255, 255, 130,  84},
-      { 78, 255,   0,   0,  78},
-      { 84, 130, 255, 130,  84}
-    };
-
-    GrayImage image = new GrayImage(pixels1);
-    System.out.println(image.countWhitePixels());
-
-    int[][] pixels2 = {
-      {221, 184, 178,  84, 135},
-      { 84, 255, 255, 130,  84},
-      { 78, 255,   0,   0,  78},
-      { 84, 130, 255, 130,  84}
-    };
-
-    image = new GrayImage(pixels2);
-    System.out.println();
-    System.out.println(image);
-    image.processImage();
-    System.out.println();
-    System.out.println(image);
-  }
+public class Main
+{
+	public static void main(String[] args)
+	{
+		//Part A
+		System.out.println("Part A");
+		String[] wordArray = {"wheels", "on", "the", "bus"};
+		RandomStringChooser sChooser = new RandomStringChooser(wordArray);
+		for (int k = 0; k < 6; k++)
+		{
+			System.out.print(sChooser.getNext() + " ");
+		}
+		
+		//Part B
+		System.out.println("\n\nPart B");
+		RandomLetterChooser letterChooser = new RandomLetterChooser("cat");
+		for (int k = 0; k < 4; k++)
+		{
+			System.out.print(letterChooser.getNext());
+		}
+	}
 }
